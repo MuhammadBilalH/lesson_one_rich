c538b2b7b5fd4c58114092399616f52f88b9cdd0
FADED <mbilalhameed6@gmail.com>
2020-08-06 14:19:37 +0500

    Day 2 Commit Used Scafold for the app. Added images from assets and url. Changed App icons for android and ios. More insight on widget tree.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Row(      // Row stacks horizontally while Column stacks Vertically
-          children: <Widget>[Column(
-            children: <Widget>[Text("r1,c1"), Text('r2,c1')],
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(
+              image: AssetImage('Assets/Images/abc.jpg'),
+            ),
           ),
-          Column(children: <Widget>[Text("r1,c2"), Text('r2,c2')],
-          ),
-          ],  // Providing list of widgets to be stacked
         ),
       ),
     ),
