7e2c477013e5bd85ee937cc08eb2fac815755420
FADED <mbilalhameed6@gmail.com>
2020-08-07 21:19:24 +0500

    Day 2 Commit Used Scafold for the app. Added images from assets and url. Changed App icons for android and ios. More insight on widget tree.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Clone app of I am rich
 ## Getting Started
 
 This project is a starting point for a Flutter application.
-
+Coal icon is from icons8.com
 A few resources to get you started if this is your first Flutter project:
 
 - [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[300],
-        appBar: AppBar(
-          title: Text('I am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image(
-              image: AssetImage('Assets/Images/abc.jpg'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[600],
+      appBar: AppBar(
+        title: Center(child: Text("I am poor")),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child:
+        Column(
+          children:
+          <Widget>[
+            Center(
+              child: Text("PLZ HELP ME !"),
             ),
-          ),
+            Center(
+              child: Image(image: AssetImage('Assets/Images/coal.png')),
+            ),
+            Center(
+              child: Image(image: AssetImage('Assets/Images/woods.jpg')),
+            )
+          ],
         ),
       ),
     ),
-  );
+  ));
 }
